b623aa5df8b502ee1ae6187503382f586defe844
dia-meTr <diana.tryhub@gmail.com>
2023-03-06 15:16:38 +0200

    Dockerfile added

--- lab1/services/UserInfoService/app/__init__.py
+++ lab1/services/UserInfoService/app/__init__.py
@@ -15,7 +15,7 @@ api = Api(app)
 
 app.config.update({
     'APISPEC_SPEC': APISpec(
-        title='Awesome Project',
+        title='User Info',
         version='v1',
         plugins=[MarshmallowPlugin()],
         openapi_version='2.0.0'
---
+++ lab1/services/UserInfoService/main.py
@@ -0,0 +1,9 @@
+import logging
+from app import app
+
+if __name__ == '__main__':
+    open('app.log', 'a').close()
+    # logging.basicConfig(filename='app.log', level=logging.DEBUG)
+
+    app.run(debug=False, host='0.0.0.0')
+
