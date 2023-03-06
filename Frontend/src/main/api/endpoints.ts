fae708cb40d3be0a913ce9218f21c3a02f5a8e1f
Bohdan Kotsiuba <bohdan.kotsiuba@gmail.com>
2023-03-06 23:49:46 +0200

    FIX Add axios client init. Add endpoints enum

---
+++ Frontend/src/main/api/endpoints.ts
@@ -0,0 +1,13 @@
+/**
+    Replace with .env in future if possible
+ */
+enum Endpoints {
+    SHORTENER_URL = "http://localhost:8080/",
+    API_URL = "http://localhost:8080/",
+    PATH_LOGIN = "login",
+    PATH_LINKS = "api/links",
+    PATH_USERS = "json_users",
+    PATH_SCHEDULER = "renew"
+}
+
+export default Endpoints;
