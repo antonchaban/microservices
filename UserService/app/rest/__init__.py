4ae56a44f058f92415c5a337245c0e888863fa2d
Bohdan Kotsiuba <bohdan.kotsiuba@gmail.com>
2023-03-08 17:28:51 +0200

    Add health check /health endpoint to all services and frontend

--- UserService/app/rest/__init__.py
+++ UserService/app/rest/__init__.py
@@ -1,1 +1,2 @@
 import app.rest.user_api
+import app.rest.health_check_api
---
+++ UserService/app/rest/health_check_api.py
@@ -0,0 +1,12 @@
+from flask_restful import Resource, marshal_with
+from flask_apispec.views import MethodResource
+
+from app import api, db, docs
+
+class GetHealth(MethodResource, Resource):
+    def get(self):
+        return None, 200
+
+api.add_resource(GetHealth, '/health')
+
+docs.register(GetHealth)
